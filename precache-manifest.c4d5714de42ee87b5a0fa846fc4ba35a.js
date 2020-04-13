1c633d18e1336c67c4103feef2cc82bba538e106
Patryk Krajewski <patrykraj@interia.pl>
2020-04-13 14:28:52 +0200

    Updates

---
+++ precache-manifest.c4d5714de42ee87b5a0fa846fc4ba35a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "394adfba0b09387908684da02c6819ee",
+    "url": "/random-data-app/index.html"
+  },
+  {
+    "revision": "711435814a9bb3fae8a5",
+    "url": "/random-data-app/static/css/2.ba989b92.chunk.css"
+  },
+  {
+    "revision": "6fd4605496c15c5ead53",
+    "url": "/random-data-app/static/css/main.b5c2a4ee.chunk.css"
+  },
+  {
+    "revision": "711435814a9bb3fae8a5",
+    "url": "/random-data-app/static/js/2.032ba854.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/random-data-app/static/js/2.032ba854.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fd4605496c15c5ead53",
+    "url": "/random-data-app/static/js/main.e8fdd9ea.chunk.js"
+  },
+  {
+    "revision": "6d80d8586702e0fd1f7f",
+    "url": "/random-data-app/static/js/runtime-main.b9e7bc7f.js"
+  },
+  {
+    "revision": "024196716802e272adb3cbefb81fd557",
+    "url": "/random-data-app/static/media/react-router.02419671.svg"
+  }
+]);
